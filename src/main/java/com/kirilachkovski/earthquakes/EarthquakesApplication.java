7e0b13a85d9194ff787594cdcb8359f3fbbd5031
achkovski <kirilackovski17@gmail.com>
2026-04-14 18:02:10 +0200

    Initial commit

---
+++ src/main/java/com/kirilachkovski/earthquakes/EarthquakesApplication.java
@@ -0,0 +1,18 @@
+package com.kirilachkovski.earthquakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Project: SeismoScore - Earthquake Monitoring System
+ *
+ * @author Kiril Achkovski
+ */
+@SpringBootApplication
+public class EarthquakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakesApplication.class, args);
+    }
+
+}
