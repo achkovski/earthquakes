9fd37823eb2cacaf58e1e1f23044181e37392401
achkovski <kirilackovski17@gmail.com>
2026-04-14 18:47:16 +0200

    IntelliJ code review changes

--- backend/src/main/java/com/kirilachkovski/earthquakes/EarthquakesApplication.java
+++ backend/src/main/java/com/kirilachkovski/earthquakes/EarthquakesApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EarthquakesApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(EarthquakesApplication.class, args);
     }
 
